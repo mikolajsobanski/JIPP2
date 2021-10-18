ead5321ebd7d196da512c59f8f098e10323def95
Mikolaj Sobanski <mikolajsobanski01@gmail.com>
2021-10-18 14:30:19 +0200

    Lab 3

---
+++ firstLib/src/lib.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../include/lib.h"
+
+using namespace std;
+
+void itIsWork (){
+    cout << "Yes it work" << endl;
+}
---
+++ firstLibCall/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "lib.h"
+
+using namespace std;
+
+int main() {
+    itIsWork();
+    return 0;
+}
